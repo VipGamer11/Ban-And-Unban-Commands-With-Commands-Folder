e3a8f427412d5e32f835ce07f77843c57d4feb60
VipGamer <90019482+VipGamer11@users.noreply.github.com>
2021-10-03 11:25:27 +0530

    Create ban.js

---
+++ ban.js
@@ -0,0 +1,35 @@
+module.exports = {
+      name: "ban",
+    code: `$if[$serverChannelExists[$getServerVar[modlogs]]==true]
+$channelSendMessage[$getServerVar[modlogs];<@$authorID>{title:Mod Logs}{field:Action:Ban}{field:Moderator:$username}{field:User:$username[$findUser[$message[1]]]#$discriminator[$findUser[$message[1]]]\n(\`$findUser[$message[1]]\`)}{thumbnail:$userAvatar[$findUser[$message[1]]]}{color:RANDOM}]
+$endif
+
+ $color[RANDOM] 
+$author[🌈 Banned successfully]
+$addField[About:;
+Reason:
+> $replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;A reason wasn't provided.];false;$messageSlice[1]]
+Date:
+> $day $month $year
+]
+$addField[User information;
+$userTag[$findUser[$message[1]]] - $findUser[$message[1]]]
+$addField[Moderator;
+$userTag - $authorID]
+$thumbnail[$userAvatar[$findUser[$message[1]]]]
+
+$ban[$findUser[$message[1]];$userTag: $replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;A reason wasn't provided.];false;$messageSlice[1]];7]
+$if[$memberExists[$findUser[$message[1]]]==true]
+$onlyIf[$rolePosition[$highestRole[$findUser[$message[1]]]]>$rolePosition[$highestRole];⛔ - To use this you need to have a higher rank than the mentioned user.]
+$onlyIf[$findUser[$message[1]]!=$authorID;**⛔ - You can't ban yourself (Or else, I couldn't find that user)**]
+$onlyIf[$findUser[$message[1]]!=$clientID;**⛔ - I can't ban myself, that's illegal**]
+$onlyIf[$findUser[$message[1]]!=$ownerID;**⛔ - I can't ban the owner of the server**]
+$elseIf[$memberExists[$findUser[$message[1]]]==false]
+$onlyIf[$findUser[$message[1]]!=$authorID;**⛔ - You can't ban yourself (Or else, I couldn't find that user)**]
+$endelseIf
+$endif
+$onlyIf[$isBanned[$findUser[$message[1]]]==false;**⛔ - This user has already been banned on this server**]
+$onlyIf[$message!=;**⛔ - Please specify the user you want to ban. Correct usage:** \`$getServerVar[prefix]ban <@User> [Reason\\]\`]
+$onlyPerms[ban;**⛔ - To use this you require the \`BAN_MEMBERS\` permission**]
+ $onlyBotPerms[ban;**⛔ - I don't have enough perms to execute this command. Permissions missing:** \`BAN_MEMBERS\`]`
+}
